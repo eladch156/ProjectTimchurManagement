e23935ab7851687bdcde0af07adc75a9134ade80
eladch156@gmail.com <eladch156@gmail.com>
2016-11-21 12:02:42 +0200

    Restriections system

---
+++ WebApplication1/WebApplication1/Infrastructure/AuthAbstract/AuthRestriections.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace WebApplication1.Infrastructure.AuthAbstract
+{
+    public class AuthRestriections : AuthorizeAttribute
+    {
+        public string AccessLevel { get; set; }
+        protected override bool AuthorizeCore(HttpContextBase context)
+        {
+            var isAuthorized=base.AuthorizeCore(context);
+            if (!isAuthorized)
+                return false;
+
+            string premission="";
+            string username = FormsAuthentication.GetAuthCookie().Value();
+                // get premission levels
+
+            if ()
+        }
+        
+    }
+}
