edb7b6b0d1bac98fd6b1853f5fd3304a9cf804fe
eladch156@gmail.com <eladch156@gmail.com>
2016-11-29 20:21:09 +0200

    Missing files

---
+++ WebApplication1/WebApplication1/Database/SingletonDatabase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Database
+{
+    public class SingletonDatabase
+    {
+        private static Database _instance;
+
+
+
+        // Constructor is 'protected'
+
+        protected SingletonDatabase() 
+
+        {
+
+        }
+
+
+
+        public static Database Instance()
+
+        {
+
+            // Uses lazy initialization.
+
+            // Note: this is not thread safe.
+
+            if (_instance == null)
+
+            {
+
+                _instance = new Database();
+
+            }
+
+
+
+            return _instance;
+
+        }
+
+    }
+}
