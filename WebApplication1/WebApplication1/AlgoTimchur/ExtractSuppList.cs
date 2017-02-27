d1d9ae03292b7b8d096925b97ccf9028f0134d88
shai <shaiofer106@gmail.com>
2017-02-28 00:26:15 +0200

    Added tichuring algorithm

---
+++ WebApplication1/WebApplication1/AlgoTimchur/ExtractSuppList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Linq;
+using WebApplication1.Database;
+
+/**
+ * A) Find all the suppliers relevant to the given cluster
+ * B) Sort them into groups based on latest extraction date regarding said cluster.
+ * C) While date-groups can be extracted fulled into the list of "polled" suppliers, do so.
+ * D) Once a date-group can only extracted partially, do so randomly.
+ */
+namespace WebApplication1.AlgoTimchur
+{
+    public class ExtractSuppList : PullSuppList
+    {
+        public TablePullResult TichurAlgorithem(TichurInfo input)
+        {
+            using (TimchurDatabaseEntities ent = new TimchurDatabaseEntities())
+            {
+               // Checking for duplicate tichurs
+               Database.Tichurim check = ent.Tichurim.Find(input.TichurNumber);
+               if (check != null && check.UnitID == input.UnitID && check.StatusID == 1)
+                {
+                    throw new DuplicateKeyException(check);
+                }
+                TablePullResult res = new TablePullResult();
+                int suppliersRemaining = ent.Clusetrs.Find(input.CluestrID).SuppliersInTichur.Value;
+
+                var sortByUsage = from sup in ent.Suppliers
+                         join supcluc in ent.SuppliersClusetrs
+                         on sup.ID equals supcluc.SupplierID
+                         where supcluc.ClusetrID == input.CluestrID && sup.StatusID == 1
+                         orderby supcluc.LastTimeInList
+                         group sup by new { supcluc.LastTimeInList, supcluc.SupplierID} into dateGroups
+                         orderby dateGroups.Key.LastTimeInList
+                         select dateGroups;
+                //Date-grouped iterator
+                var itr = sortByUsage.GetEnumerator();
+                var rnd = new Random();
+                while (suppliersRemaining > 0)
+                {
+                    //oldest group of suppliers
+                    var dateGroup = itr.Current;
+                    //Need to pick a random subset of the suppliers
+                    if (dateGroup.Count() < suppliersRemaining)
+                    {
+                        var shuffled = dateGroup.OrderBy(i => rnd.Next());
+                        // Going over the shuffled list until enough are extracted
+                        foreach (var supplier in shuffled)
+                        {
+                            res.table.Add(supplier.ToString());
+                            if (--suppliersRemaining == 0)
+                            {
+                                break;
+                            } 
+                        }
+                    } else
+                    {
+                        //Need at least one entire dategroup - adding all suppliers in current set.
+                        foreach (var supplier in dateGroup)
+                        {
+                            res.table.Add(supplier.ToString());
+                        }
+                        suppliersRemaining -= dateGroup.Count();
+                        itr.MoveNext();
+                    }
+                }
+                return res;
+            }
+        }
+    }
+}
--- WebApplication1/WebApplication1/AlgoTimchur/TablePullResult.cs
+++ WebApplication1/WebApplication1/AlgoTimchur/TablePullResult.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
 
+/***
+ * This class is returned as a result of a data-extraction algorithm,
+ * with each item on the list pertaining to the ID/Key value of the element selected.
+ */
 namespace WebApplication1.AlgoTimchur
 {
     public class TablePullResult
     {
-        public List<string[]> table; 
-        /***
-        הכוונה שלי כאן הייתה להמיר את הנתונים שאתה מקבל לאיזה שהוא פורמט של טבלה שאני יוכל
-        להמיר לג'יסון בקלות
-        ***/
+        public List<string> table = new List<string>(); 
     }
 }
