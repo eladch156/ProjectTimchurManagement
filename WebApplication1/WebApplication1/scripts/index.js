1864a082bb8f77b3727dd55a72bd9e4370396c33
eladch156@gmail.com <eladch156@gmail.com>
2017-05-29 17:14:44 +0300

    Addition of some bug fixes

--- WebApplication1/WebApplication1/scripts/index.js
+++ WebApplication1/WebApplication1/scripts/index.js
@@ -4,6 +4,28 @@ Originally found at https://cssdeck.com/labs/login-form-using-html5-and-css3
 by: https://cssdeck.com/user/kamalchaneman
 
 
-
+Free Source Confirmed
 
 */
+window.addEventListener('load', function (event) {
+    var caps = event.getModifierState && event.getModifierState('CapsLock');
+    if (caps) {
+        document.getElementById("ca_indi").innerHTML = "Caps Lock is : On";
+        document.getElementById("ca_indi").style.color = "green";
+    }
+    else {
+        document.getElementById("ca_indi").innerHTML = "Caps Lock is : Off";
+        document.getElementById("ca_indi").style.color = "red";
+    }
+});
+document.addEventListener('keydown', function (event) {
+    var caps = event.getModifierState && event.getModifierState('CapsLock');
+    if (caps) {
+        document.getElementById("ca_indi").innerHTML = "Caps Lock is : On";
+        document.getElementById("ca_indi").style.color = "green";
+    }
+    else {
+        document.getElementById("ca_indi").innerHTML = "Caps Lock is : Off";
+        document.getElementById("ca_indi").style.color = "red";
+    }
+});
