6fa6a28f13f1aee329e350c17b9f07c08ae756c4
shai <shai@LENOVOFER>
2017-06-20 15:01:36 +0300

    Modified error messages for readability

--- WebApplication1/WebApplication1/Controllers/MainController.cs
+++ WebApplication1/WebApplication1/Controllers/MainController.cs
@@ -705,7 +705,7 @@ namespace WebApplication1.Controllers
                 {
                     GenModel failRes = new GenModel();
                     failRes.data = new List<string[]>();
-                    failRes.data.Add(new string[] { "תיחור אינו קיים בתוך השרת (או תחת היחידה הנתונה של המשתמש או שהוכנסה)" });
+                    failRes.data.Add(new string[] { "תיחור אינו קיים ביחידה של המשתמש" });
                     failRes.Status = "error";
                     return Json(failRes); 
                 }
@@ -796,7 +796,6 @@ namespace WebApplication1.Controllers
         [AuthRestriections(Name = "/Main/TichurExisting")]
         public ActionResult GetByTichurDates(TEBDData data)
         {
-        
             DateTime to;
             string[] to_str = data.To.Split('/');
             to = new DateTime(Int32.Parse(to_str[2]), Int32.Parse(to_str[0]), Int32.Parse(to_str[1]));
